ddf942964c4d8cc7f707eaccf8891a921ecb3d34
john <biboc233@163.com>
2025-10-24 19:40:04 +0800

    implemented some build-in command next step is to learn sth about thread and process

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 int main(int argc, char**argv){
+    //>>>>>>>>>>>>>>>>>>>>>>>>>initilize<<<<<<<<<<<<<<<<<<<<<<
     char *reader=(char*)malloc(sizeof(char)*BUFFER_SIZE);
     char * current_path=(char*)malloc(sizeof(char)*BUFFER_SIZE);
 
@@ -15,12 +16,39 @@ int main(int argc, char**argv){
         stringstream ss(reader);
         string command;
         ss >> command;
+
+        //>>>>>>>>>>>>>>>>>>>build in command<<<<<<<<<<<<<<<<<<
         if(command=="echo"){
             string arg=ss.str().substr(5);
             echo_command(arg);
             continue;
         }
-        
+
+        else if(command=="exit"){
+            break;
+        }
+
+        else if(command=="pwd"){
+            char pwd_buffer[BUFFER_SIZE];
+            getcwd(pwd_buffer,BUFFER_SIZE);
+            cout<<pwd_buffer<<endl;
+            continue;
+        }
+
+        else if(command=="cd"){
+            string path;
+            ss >> path;
+            if(path.empty()){
+                if(chdir(getenv("HOME"))!=0){
+                    cerr<<"cannot open home directoty"<<endl;
+                }
+            }
+            else{
+                if(chdir(path.c_str())!=0){
+                    cerr<<"no such file or directory: "<<path<<endl;
+                }
+            }
+        }
     }
     return 0;
 }
